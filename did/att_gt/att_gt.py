0ca63f74498a3ea593878e3e227b8de3e8a0e3ce
tjhon <fr.jhonk@gmail.com>
2023-05-25 10:25:46 -0500

    pre_process_did

--- did/att_gt/att_gt.py
+++ did/att_gt/att_gt.py
@@ -9,5 +9,36 @@ from pre_process_did import pre_process_did
 from compute_attgt import compute_att_gt
 from process_attgt import process_attgt
 
-def att_gt() -> list:
-  pass
+def process_attgt(
+    yname,
+    tname,
+    gname,
+    data,
+    idname=None,
+    xformla=None,
+    panel=True,
+    allow_unbalanced_panel=False,
+    control_group=["nevertrated", "notyettreated"],
+    anticipation=0,
+    weightsname=None,
+    alpha=0.05,
+    bstrap=True,
+    cband=True,
+    cband=True,
+    biters=1000,
+    clustervars=None,
+    est_method="dr",
+    base_period="varying",
+    print_details=False,
+    pl=False,
+    cores=1,
+  ) -> list:
+  dp = pre_process_did(
+    yname,
+    tname,
+    idname,
+    gname,
+    data,
+    panel,
+    
+  )
--- did/att_gt/pre_process_did.py
+++ did/att_gt/pre_process_did.py
@@ -1,4 +1,59 @@
-from .DID_params import DIDparams
+import pandas as pd, numpy as np
+from DID_params import DIDparams
 
-def pre_process_did() -> list:
-  pass
+def pre_process_did(
+  yname, tname, idname, gname, data: pd.DataFrame, 
+  xformla = None, weights_names = None, 
+  control_group = ['nevertreated']) -> list:
+
+  control_group = control_group[0]
+  if control_group not in ["nevertreated", "notyettreated"]:
+    raise ValueError("control_group must be either 'nevertreated' or 'notyettreated'")
+
+  # make sure dataset is a DataFrame
+  # this gets around Python's default of reading data as a different data structure
+  # if not isinstance(data, pd.DataFrame):
+  #   data = pd.DataFrame(data)
+
+  # make sure time periods are numeric
+  if not data[tname].dtype == "float64":
+    raise ValueError("data[tname] must be numeric")
+
+  # make sure gname is numeric
+  if not data[gname].dtype == "float64":
+    raise ValueError("data[gname] must be numeric")
+
+  # put in blank xformla if no covariates
+  if xformla is None:
+    xformla = "~1"
+  
+  columns = [idname, tname, yname, gname, weightsname, clustervars]
+  #  drop irrelevant columns from data
+  # data <- cbind.data.frame(data[,c(idname, tname, yname, gname, weightsname, clustervars)], model.frame(xformla, data=data, na.action=na.pass))
+
+  data = data[columns]
+  
+  n_orig, c_orig = data.shape
+
+  data = data.dropna()
+  n_diff = n_orig - data.shape[0]
+  if weights_names is None: 
+    w = np.ones(data.shape[0])
+  else:
+    w = data[weights_names]
+
+
+  if "w" in data.columns:
+    raise ValueError("`did` tried to use column named 'w' internally, but there was already a column with this name")
+
+  data = data.assign(w = w)
+  tlist = data[tname].unique()
+  tlist = sorted(tlist)
+
+  asif_never_treated = data[gname] > max(tlist)
+  asif_never_treated.fillna(False, inplace=True)
+  data.loc[asif_never_treated, gname] = 0
+
+  glist = data[gname].unique()
+  glist = sorted(glist)
+  
